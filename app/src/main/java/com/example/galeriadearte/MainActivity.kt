04424a19d18c90c9adc9f0bbe40a47c82e1d7c6a
alanmmartins <alanmunoz@gmail.com>
2023-05-18 17:26:45 -0300

    atualizçao18/05

--- app/src/main/java/com/example/galeriadearte/MainActivity.kt
+++ app/src/main/java/com/example/galeriadearte/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,35 +37,80 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-@Composable
+
+    @Composable
     fun AppGaleriaDeArte() {
+        var result = 1
 
+        val imageResource = when (result) {
 
-    Image(painter = painterResource(id = R.drawable.vangogh1 ),
-        contentDescription = )
-
-    Text(
-        text = "Previous",
-        modifier = Modifier,
-        fontSize = 40.sp,
-        fontWeight = FontWeight.Light
-    )
-
-    Text(
-        text = "Next",
-        modifier = Modifier,
-        fontSize = 40.sp,
-        fontWeight = FontWeight.Light
-
-    )
-
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .size(30.dp)
+            1 -> R.drawable.vangogh1
+            2 -> R.drawable.cachorro
+            3 -> R.drawable.vangogh
+            4 -> R.drawable.download
+            5 -> R.drawable.vangog3
+            else -> R.drawable.vangog3
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .size(30.dp)
+        ) {
+            
+            
+            Image(
+                painter = painterResource(id = R.drawable.vangogh1),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(450.dp)
             )
 
+            Text(
+                text = "Vicent Van Gogh",
+                modifier = Modifier,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Black
+            )
 
+            Text(
+                text = "Obras",
+                modifier = Modifier,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Light
+            )
+            Row() {
+                Button(onClick = {},
+                    modifier = Modifier
+                        .padding(60.dp)
+                ) {
+                    Text(
+                        text = "Previous")
+                }
+            Button(onClick = {},
+                modifier = Modifier
+                    .padding(60.dp)
+            ) {
+                Text(
+                    text = "Next")
+            }
+
+            }
+        }
     }
+}
+@Composable
+fun MudarImagem (recursoTextoId : Int, recursoImagemId : Int, onImagemClick : ()->Unit) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    )
+    {
+    }
+}
+
+
+
